46ee6a7a63824286c4d1b2c061a864d641922013
Kapcari-Kuhn Edite <kapedi@orbis.de>
2017-07-24 09:11:13 +0200

    Added css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import './App.css'
 
 class App extends Component {
     render() {
         return (
-            <div>Countdown Show, App component</div>
+            <div className="App">
+                <div className='App-title'>Countdown to December 25, 2017</div>
+                <div>
+                    <div className='Clock-days'>14 Days</div>
+                    <div className='Clock-hours'>31 Hours</div>
+                    <div className='Clock-minutes'>45 Minutes</div>
+                    <div className='Clock-seconds'>13 Seconds</div>
+                </div>
+                <div>
+                    <input placeholder="new date" />
+                    <button>Submit</button>
+                </div>
+            </div>
         )
     }
 }
